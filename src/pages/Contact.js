ffd66c40cdc5509e5d5a2af90993b5ec8cc22356
royce-u <royceubando@gmail.com>
2020-06-23 16:30:12 -1000

    swapped resume

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,7 +21,7 @@ const About = () => {
                     
                     <p><a href="https://github.com/royce-u/" target="_blank"><Icon color="grey" name="github" size="massive" className="link-clean" /></a><h4>Github</h4></p> 
                     <p><a href="https://www.linkedin.com/in/royce-ubando/" target="_blank"><Icon color="grey" name="linkedin" size="massive" className="link-clean" /></a><h4>LinkedIn</h4></p> 
-                    <p><a href="../ATS_Resume_final.pdf" target="_blank"><Icon color="grey" name="file" size="massive" className="link-clean" /></a><h4>Resume</h4></p> 
+                    <p><a href="../Design_Resume.pdf" target="_blank"><Icon color="grey" name="file" size="massive" className="link-clean" /></a><h4>Resume</h4></p> 
                 </Grid.Row>
                 <Grid.Row><h3 className="header-txt bg-grey w-hunnit">Technical Skills</h3></Grid.Row>
                 <Grid.Row >
