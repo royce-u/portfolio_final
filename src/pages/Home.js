0d87aaa5266038f6ddadf5cd7dddd39e1ffff2fe
royce-u <royceubando@gmail.com>
2020-06-23 18:42:42 -1000

    fixed homepage banner spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Container, Grid} from 'semantic-ui-react'
+import { Container, Grid } from 'semantic-ui-react'
 
 const Home = props => {
     return (
-        <Container className="fullSize header-top">
-            <Grid.Row>
-                <Grid.Column><h1 id="home" className="header-top homepage">Royce Ubando</h1></Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column sm={4}></Grid.Column>
-                <Grid.Column sm={8}><h3 className="hp-title">Full Stack Developer</h3></Grid.Column>
-            </Grid.Row>
+        <Container className="fullSize header-top" id="home">
+    
+            <br />
+            <br />
+            <br />
+            <br />
+                <Grid.Row >
+                    <Grid.Column><h1 className="header-top homepage">Royce Ubando</h1></Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column sm={4}></Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column sm={10}><h3 className="hp-title" >Full Stack Developer</h3></Grid.Column>
+                    </Grid.Row>
+                </Grid.Row>
+         
         </Container>
-        
+
     )
 }
 
