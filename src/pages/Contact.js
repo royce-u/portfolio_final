46b97e82e7b3aae23bcd0571ea20ab94af5f98ea
royce-u <royceubando@gmail.com>
2020-08-19 21:27:24 -0700

    file cleanup

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,7 +14,7 @@ const About = () => {
     // }
     return (
         <Container className="f-vh mt-10" id="contact">
-            <Grid columns={1} stackable>
+            <Grid stretched>
                 <Grid.Row>
                     <h1 className="header-txt w-hunnit">Contact</h1>
                 </Grid.Row>
@@ -24,17 +24,17 @@ const About = () => {
                     <p><a href="https://www.linkedin.com/in/royce-ubando/" target="_blank"><Icon color="grey" name="linkedin" size="massive" className="link-clean" /></a><h4>LinkedIn</h4></p>
                     <p><a href="../Design_Resume.pdf" target="_blank"><Icon color="grey" name="file" size="massive" className="link-clean" /></a><h4>Resume</h4></p>
                 </Grid.Row>
-                <Grid.Row><h3 className="header-txt bg-grey w-hunnit">Technical Skills</h3></Grid.Row>
+                <Grid.Row><h3 className="header-txt w-hunnit">Technical Skills</h3></Grid.Row>
                 <Grid.Row >
-                    <Image src="../javascript.svg" size="tiny" />
-                    <Image src="../005-python.svg" size="tiny" />
-                    <Image src="../006-react.svg" size="tiny" />
-                    <Image src="../025-typescript.svg" size="tiny" />
-                    <Image src="../css.svg" size="tiny" />
-                    <Image src="../html-5.svg" size="tiny" />
-                    <Image src="../nodejs.svg" size="tiny" />
-                    <Image src="../sequelize2.svg" size="tiny" />
-                    <Image src="../mongodb.svg" size="tiny" className="bg-white" />
+                    <Image src="../images/javascript.svg" size="tiny" />
+                    <Image src="../images/005-python.svg" size="tiny" />
+                    <Image src="../images/006-react.svg" size="tiny" />
+                    <Image src="../images/025-typescript.svg" size="tiny" />
+                    <Image src="../images/css.svg" size="tiny" />
+                    <Image src="../images/html-5.svg" size="tiny" />
+                    <Image src="../images/nodejs.svg" size="tiny" />
+                    <Image src="../images/sequelize2.svg" size="tiny" />
+                    <Image src="../images/mongodb.svg" size="tiny" className="bg-white" />
                 </Grid.Row>
             </Grid>
         </Container>
